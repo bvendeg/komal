2df224db1a5317edef42868bd364ceaec64015fa
Balazs Vendeg <balazs.vendeg@gamma.co.uk>
2017-10-30 14:47:10 +0100

    Remember the last denomination index.

--- denominations/denom.py
+++ denominations/denom.py
@@ -7,9 +7,10 @@ class Denominator(object):
     def __init__(self):
         self._DENOMINATIONS = [20000, 10000, 5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5, 1]
         self._price = 0
+        self._denom_index = -1
 
     def calc_min_denoms(self, price):
-        self._price = price
+        self._reset(price)
         payment = []
         if self._price > 0:
             denomination, multiplicity = self.find_largest_denom()
@@ -18,9 +19,12 @@ class Denominator(object):
         return payment
 
     def find_largest_denom(self):
-        denom_index = -1
         int_part = 0
         while int_part <= 0:
-            denom_index += 1
-            int_part = self._price / self._DENOMINATIONS[denom_index]
-        return self._DENOMINATIONS[denom_index], int_part
+            self._denom_index += 1
+            int_part = self._price / self._DENOMINATIONS[self._denom_index]
+        return self._DENOMINATIONS[self._denom_index], int_part
+
+    def _reset(self, price):
+        self._price = price
+        self._denom_index = -1
